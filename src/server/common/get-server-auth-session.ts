d76ca36204ef0bd764f9c3d421706796f09ba52f
Guillermo Cava <gcavanunez@gmail.com>
2022-12-21 16:31:29 -0500

    feat: simple iron session implementation

---
+++ src/server/common/get-server-auth-session.ts
@@ -0,0 +1,20 @@
+import { getIronSession } from "iron-session";
+import { type GetServerSidePropsContext } from "next";
+
+/**
+ * Wrapper for unstable_getServerSession https://next-auth.js.org/configuration/nextjs
+ * See example usage in trpc createContext or the restricted API route
+ */
+export const getServerAuthSession = async (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return await getIronSession(ctx.req, ctx.res, {
+    cookieName: "myapp_cookiename",
+    password: "complex_password_at_least_32_characters_long",
+    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  });
+};
--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,9 +1,11 @@
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-
+import bcrypt from "bcrypt";
+import crypto from "crypto";
 import { router, publicProcedure } from "../trpc";
 
 export const authRouter = router({
+  // getUser: publicProcedure.input()
   login: publicProcedure
     .input(
       z.object({
@@ -21,12 +23,16 @@ export const authRouter = router({
       if (!user) {
         throw new TRPCError({ code: "BAD_REQUEST", cause: "User not found" });
       }
+
       if (user.password !== input.password) {
         throw new TRPCError({
           code: "BAD_REQUEST",
           cause: "Hey your password is wrong",
         });
       }
+
+      ctx.session["user"] = { id: user.id };
+      await ctx.session.save();
 
       return {
         user,
@@ -50,6 +56,51 @@ export const authRouter = router({
 
       return {
         message: "Success",
+      };
+    }),
+  passwordEmail: publicProcedure
+    .input(
+      z.object({
+        email: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      /**
+       * 1. check user exists
+       * 2. remove any older tokens
+       * 3. generate a hash
+       * 4. create a hash and persist it to the database (should send email)
+       * 5. return a sucess message
+       */
+      const user = await ctx.prisma.user.findFirst({
+        where: {
+          email: input.email,
+        },
+      });
+
+      if (!user) {
+        throw new TRPCError({ code: "BAD_REQUEST", cause: "User not found" });
+      }
+
+      await ctx.prisma.passwordReset.delete({
+        where: {
+          email: input.email,
+        },
+      });
+
+      const char = crypto.randomBytes(20).toString("hex");
+      const salt = await bcrypt.genSalt(10, "b");
+      const token = await bcrypt.hash(char, salt);
+
+      await ctx.prisma.passwordReset.create({
+        data: {
+          email: input.email,
+          token,
+        },
+      });
+
+      return {
+        message: "Password created",
       };
     }),
 });
